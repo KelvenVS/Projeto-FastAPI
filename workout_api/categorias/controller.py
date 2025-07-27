965983b0c3b8703051279fde01032b5980b83cb4
KelvenVS <kelvenserejo@gmai.com>
2025-07-27 13:58:13 -0300

    📚 feat: adicionar rotas GET e GET by ID para categorias
    - Criadas duas novas rotas no `controller` de categorias:
      - `GET /categorias/` → retorna todas as categorias cadastradas (`CategoriaOut[]`).
      - `GET /categorias/{id}` → retorna uma categoria específica pelo `UUID`, com validação de existência.
    
    - Utilizado `select()` da SQLAlchemy com `db_session.execute(...).scalars().all()` e `.first()` para busca eficiente.
    - Erros tratados com `HTTPException 404` para categoria não encontrada.

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -1,8 +1,10 @@
 from uuid import uuid4
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, HTTPException, Body, status
+from pydantic import UUID4
 from workout_api.categorias.models import CategoriaModel
 from workout_api.categorias.schemas import CategoriaIn , CategoriaOut
 from workout_api.contrib.dependencies import DatabaseDependency
+from sqlalchemy.future import select
 router = APIRouter()
 
 @router.post(
@@ -22,3 +24,28 @@ async def post(
     await db_session.commit()
     
     return categoria_out
+
+@router.get(
+    path='/',
+    summary='Consultar todas as categorias',
+    status_code=status.HTTP_200_OK,
+    response_model=list[CategoriaOut],
+)
+async def query(db_session: DatabaseDependency,) -> list[CategoriaOut]:
+    categorias: list[CategoriaOut] = (await db_session.execute(select(CategoriaModel))).scalars().all()
+    
+    return categorias
+
+@router.get(
+    path='/{id}',
+    summary='Consultar uma categoria pelo id',
+    status_code=status.HTTP_200_OK,
+    response_model=CategoriaOut,
+)
+async def query(id: UUID4, db_session: DatabaseDependency,) -> CategoriaOut:
+    categoria: CategoriaOut = (await db_session.execute(select(CategoriaModel).filter_by(id=id))).scalars().first()
+    
+    if not categoria:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Categoria não encontrada no i: {id}")
+    
+    return categoria
