9257f396b426263ee15eadd2aae24c7d33a40f7f
KelvenVS <kelvenserejo@gmai.com>
2025-07-27 13:34:20 -0300

    🧹 style: ajustes no controller e relacionamento entre modelos
    - Removido espaço entre o decorator `@router.post(...)` e a definição da função `async def post(...)`.
    - Corrigido nome do `back_populates` em `CategoriaModel` de `'categorias'` para `'categoria'`, mantendo consistência com `AtletaModel`.
    - Melhora a legibilidade do código e evita inconsistência nos relacionamentos ORM.

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -8,7 +8,6 @@ router = APIRouter()
     summary='Criar novo atleta',
     status_code=status.HTTP_201_CREATED
 )
-
 async def post(
     db_session: DatabaseDependency,
     atleta_in: AtletaIn = Body(...)
